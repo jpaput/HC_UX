4d432d95f6d2ef64c0e4cb1bc34a214a869ef989
Laetitia Bordier <laetitia@heetch.com>
2020-02-24 15:16:43 +0100

    Adding app/src/main/java/com/heetch/technicaltest/util/ImageDownloader.kt

---
+++ app/src/main/java/com/heetch/technicaltest/util/ImageDownloader.kt
@@ -0,0 +1,10 @@
+package com.heetch.technicaltest.util
+
+import android.graphics.Bitmap
+import io.reactivex.Observable
+
+interface ImageDownloader {
+
+    fun loadImage(url:String) : Observable<Bitmap>
+
+}
